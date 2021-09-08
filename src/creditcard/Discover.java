2f02fc6d1a8b22082f0a1a76d9e40b7c98ceb8c3
Brett Rucks <brucks24@gmail.com>
2021-09-08 08:36:25 -0500

    Small changes

--- src/creditcard/Discover.java
+++ src/creditcard/Discover.java
@@ -11,13 +11,15 @@ public class Discover extends CreditCard{
         this.interest = Math.round((balance * interestRate) * 100) / 100.0;
 
     }
-
+    @Override
     public void setCreditCardBalance(double balance) {
         this.balance = balance;
     }
+    @Override
     public void setCreditCardInterestRate(double interestRate) {
         this.interestRate = interestRate;
     }
+    @Override
     public double getCreditCardInterest() {
         return this.interest;
     }
